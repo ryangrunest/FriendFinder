6b17d40f04d782ec2aba100abf6f0bd61f9833ef
Ryan Grunest <ryangrunest45@gmail.com>
2019-04-14 21:03:40 -0700

    fixed home button

--- public/js/app.js
+++ public/js/app.js
@@ -1,28 +1,3 @@
-// alert('connected');
-$('#submit-btn').on('click', (event) => {
-    event.preventDefault();
-    console.log($('button').attr('type'));
-    if ($('button').attr('type') === 'submit') {
-        let newFriend = {
-            name: $('#name').val().trim(),
-            photo: $('#image').val().trim(),
-            q1: $('#q1').val().trim(),
-            q2: $('#q2').val().trim(),
-            q3: $('#q3').val().trim(),
-            q4: $('#q4').val().trim(),
-            q5: $('#q5').val().trim()
-        };
-        $.post("/api", newFriend).then(function(data) {
-        console.log(data);
-        alert("Reservation Added!");
-        });  
-    } else {
-        alert('not a submit btn');
-    }
-
-    
-});
-
 checkAriaExpanded = () => {
     console.log($('#dropdown-button-check').attr('aria-expanded'));
     if ($('#dropdown-button-check').attr('aria-expanded') === 'false') {
@@ -33,5 +8,28 @@ checkAriaExpanded = () => {
         $('#bottom-nav').addClass('bg-transparent'); 
     }
 }
+checkFriends = (data) => {
+    console.log(data);
+}
+$('#submit-btn').on('click', (event) => {
+    event.preventDefault();
+    console.log($('button').attr('type'));
+    let newFriend = {
+        name: $('#name').val().trim(),
+        photo: $('#image').val().trim(),
+        q1: $('#q1').val().trim(),
+        q2: $('#q2').val().trim(),
+        q3: $('#q3').val().trim(),
+        q4: $('#q4').val().trim(),
+        q5: $('#q5').val().trim()
+    };
+    $.post("/api", newFriend).then(function(data) {
+    console.log(data);
+    $.get('/api', checkFriends)
+    alert("Reservation Added!");
+    });  
+});
+
+
 
 $('#dropdown-button-check').on('click', checkAriaExpanded);
